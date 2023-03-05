b003538c6a8318867a39ae0d9859438af1a3d97b
min33sky <min33sky@naver.com>
2023-03-05 19:51:51 +0900

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import ColorPicker from './components/ColorPicker';
 import { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-slate-700">
       <ColorPicker />
+      <Footer />
       <Toaster position="top-right" />
     </div>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 py-3">
+      <div className="text-slate-300">
+        <a
+          href="https://github.com/min33sky/Image-Color-Picker"
+          target="_blank"
+          rel="noreferrer noopener"
+          title="Go to Github repository"
+          aria-label="Link to Github repository"
+        >
+          Github
+        </a>
+      </div>
+    </footer>
+  );
+}
